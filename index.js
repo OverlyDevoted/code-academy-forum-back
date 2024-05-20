9888bf90200b57fdd2a5236898c3f5d482dc626b
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-05-20 11:09:26 +0300

    Added listener

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Back");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = new express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json("OK");
+});
+
+app.listen("8080", () => {
+  console.log("Server listening on http://localhost:8080");
+});
