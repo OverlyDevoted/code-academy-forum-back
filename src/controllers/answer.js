c5b0be59d73e4b6ba0087f36cc6a0f7ec6e4581e
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-05-26 17:55:25 +0300

    Added like answer endpoint

--- src/controllers/answer.js
+++ src/controllers/answer.js
@@ -1,6 +1,7 @@
 const { v4: uuid } = require("uuid");
 const answerModel = require("../models/answer");
 const answerSchema = require("../schemas/answer");
+const { object } = require("joi");
 
 module.exports.CREATE_ANSWER = async (req, res) => {
   try {
@@ -50,7 +51,10 @@ module.exports.GET_QUESTION_ANSWERS = async (req, res) => {
         },
       ])
       .exec();
-
+    if (!answers.length)
+      return res
+        .status(400)
+        .json({ message: "There are no answers with this ID" });
     return res.json(answers);
   } catch (e) {
     console.log(e);
@@ -81,5 +85,64 @@ module.exports.DELETE_ANSWER = async (req, res) => {
     return res
       .status(500)
       .json({ message: "Something went wrong while deleting answer" });
+  }
+};
+
+module.exports.LIKE_ANSWER = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id)
+      return res.status(400).json({
+        message: "Could not like the answer because answer ID is missing",
+      });
+
+    const { user_id } = req.body;
+
+    if (!user_id)
+      return res.status(400).json({
+        message: "Could not like the answer because user ID is missing",
+      });
+
+    const { is_like } = req.body;
+
+    if (!req.body.hasOwnProperty("is_like"))
+      return res.status(400).json({
+        message: 'Could not like the answer because "is_like" is missing',
+      });
+
+    const answer = await answerModel.findOne({ id });
+
+    if (!answer)
+      return res.status(400).json({
+        message:
+          "Could not like the answer because there's no answer with such ID",
+      });
+
+    const { liked_by, disliked_by } = answer;
+
+    const likeToAdd = is_like ? liked_by : disliked_by;
+    let likeToRemove = is_like ? disliked_by : liked_by;
+    console.log(likeToAdd, likeToRemove);
+
+    if (likeToAdd.some((user) => user === user_id))
+      return res
+        .status(400)
+        .json({ message: "User has already liked this record" });
+
+    likeToAdd.push(user_id);
+    likeToRemove = likeToRemove.filter((user) => user_id !== user);
+
+    await answerModel.updateOne(
+      { id },
+      {
+        liked_by: is_like ? likeToAdd : likeToRemove,
+        disliked_by: !is_like ? likeToAdd : likeToRemove,
+      }
+    );
+    return res.json(`Successfully ${is_like ? "liked" : "disliked"} answer`);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: "Could not like an answer" });
   }
 };
--- src/routers/answer.js
+++ src/routers/answer.js
@@ -3,6 +3,7 @@ const {
   CREATE_ANSWER,
   GET_QUESTION_ANSWERS,
   DELETE_ANSWER,
+  LIKE_ANSWER,
 } = require("../controllers/answer");
 const authUser = require("../middleware/auth");
 
@@ -11,5 +12,5 @@ const answerRouter = express.Router();
 answerRouter.post("/answer", authUser, CREATE_ANSWER);
 answerRouter.get("/answer/:id", GET_QUESTION_ANSWERS);
 answerRouter.delete("/answer/:id", authUser, DELETE_ANSWER);
-
+answerRouter.patch("/answer/:id", authUser, LIKE_ANSWER);
 module.exports = answerRouter;
